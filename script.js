e66762d8fdb024cc6b2431399b71fa407a0a68ec
RomanRomaan <jay4or@gmail.com>
2025-06-21 12:23:05 +0300

    final

---
+++ script.js
@@ -0,0 +1,17 @@
+// Створити функцію, яка прибирає з рядка всі символи, які ми передали другим аргументом. 
+// 'func(" hello world", ['l', 'd'])' поверне нам "heo wor". 
+// Вихідний рядок та символи для видалення задає користувач.
+
+
+function remove_chars(string, chars) {
+    let result = '';
+    for (let i = 0; i < string.length; i++) {
+        if (chars.includes(string[i])) {
+            continue;
+        }
+        else { result += string[i] }
+    }
+    return result
+}
+console.log(remove_chars('hello', 'h'))
+
